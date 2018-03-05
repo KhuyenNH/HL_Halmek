56272dffd470e7056ac9a8db5c9aedc36307efa8
KhuyenNH <huukhuyen.dev@gmail.com>
2018-03-05 09:35:17 +0700

    update

--- res/special/2018/about_1803/js/main.js
+++ res/special/2018/about_1803/js/main.js
@@ -1,3 +1,12 @@
+/* Scrool bookmark
+-------------------------------------- */
+$('a[href^="#"]').click(function() {
+    var the_id = $(this).attr("href");
+    $('html, body').animate({
+        scrollTop: $(the_id).offset().top
+    }, 1000);
+    return false;
+});
 /* Match height
 -------------------------------------- */
 equalheight = function(container) {
